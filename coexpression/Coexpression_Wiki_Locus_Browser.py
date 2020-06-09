d47767f9c9e230009686d034b1bbfccbc66d43ce
tere104 <39619800+tere104@users.noreply.github.com>
2020-06-09 14:25:32 +0200

    Update Coexpression_Wiki_Locus_Browser.py

--- coexpression/Coexpression_Wiki_Locus_Browser.py
+++ coexpression/Coexpression_Wiki_Locus_Browser.py
@@ -1,38 +1,21 @@
 
 # coding: utf-8
 
-# In[13]:
-
 
 import pandas as pd
 
-
 # Read evidence genes
-
-# In[14]:
-
 
 genes = pd.read_csv("coexpression/genes_by_locus.csv", sep=";")
 
-
 # Read coexpression data
-
-# In[15]:
-
 
 g2pml = pd.read_csv("coexpression/G2PMLData4IPDGC.csv", sep=";")
 
 
 # Merge evidence genes and coexpression data
 
-# In[19]:
-
-
 merged = pd.merge(genes, g2pml, on="GENE")
-
-
-# In[21]:
-
 
 merged.to_csv("coexpression/Genes_g2pml_present_genes_by_locus.csv", index=False, sep=";")
 
